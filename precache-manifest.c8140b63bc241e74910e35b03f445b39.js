61536c88ab18937d5ede969fe754a3444934e0f6
Rafael De Leon <rafaeldeleon@rafaels-mbp.lan>
2020-04-11 16:14:53 -0400

    Updates

---
+++ precache-manifest.c8140b63bc241e74910e35b03f445b39.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912ce677e1cfdd4a5b0a77a7eca0452e",
+    "url": "/covid19-web/index.html"
+  },
+  {
+    "revision": "f67c185c5ecac30b7f66",
+    "url": "/covid19-web/static/css/4.d8d84b82.chunk.css"
+  },
+  {
+    "revision": "0de0fce4538526ca646e",
+    "url": "/covid19-web/static/css/main.c133766e.chunk.css"
+  },
+  {
+    "revision": "2ab7646b602a89bae529",
+    "url": "/covid19-web/static/js/2.404de4d7.chunk.js"
+  },
+  {
+    "revision": "bb07bf585b5364cfbb4632dd9308d60e",
+    "url": "/covid19-web/static/js/2.404de4d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068d84a928fdb98a6621",
+    "url": "/covid19-web/static/js/3.8513953d.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/covid19-web/static/js/3.8513953d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67c185c5ecac30b7f66",
+    "url": "/covid19-web/static/js/4.875a2aaa.chunk.js"
+  },
+  {
+    "revision": "0de0fce4538526ca646e",
+    "url": "/covid19-web/static/js/main.905f0cce.chunk.js"
+  },
+  {
+    "revision": "3780d7b77e04bb128ae5",
+    "url": "/covid19-web/static/js/runtime-main.9e77b85b.js"
+  }
+]);
