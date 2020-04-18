699a8805a93b87cb12a4fe5417b40e2ba604ae52
rafaeldeleonp <rafael.deleon@outlook.com>
2020-04-18 13:30:15 -0400

    Updates

---
+++ precache-manifest.6238515374120f921943db02a30752a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a6ac1e52cd477548272473810ce51b",
+    "url": "/covid19-web/index.html"
+  },
+  {
+    "revision": "79b3deca238a2a63dad9",
+    "url": "/covid19-web/static/css/4.671f3c09.chunk.css"
+  },
+  {
+    "revision": "cd6188f442c656f6bd1c",
+    "url": "/covid19-web/static/css/main.677dcf71.chunk.css"
+  },
+  {
+    "revision": "4acbc48a596ee8823b95",
+    "url": "/covid19-web/static/js/2.6b6db4bb.chunk.js"
+  },
+  {
+    "revision": "bb07bf585b5364cfbb4632dd9308d60e",
+    "url": "/covid19-web/static/js/2.6b6db4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb6df45a0b83f2e5139",
+    "url": "/covid19-web/static/js/3.14d810ab.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/covid19-web/static/js/3.14d810ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b3deca238a2a63dad9",
+    "url": "/covid19-web/static/js/4.0e3106d7.chunk.js"
+  },
+  {
+    "revision": "cd6188f442c656f6bd1c",
+    "url": "/covid19-web/static/js/main.49cbf1ae.chunk.js"
+  },
+  {
+    "revision": "fedb8f8e03957c169918",
+    "url": "/covid19-web/static/js/runtime-main.3b161c78.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-web/precache-manifest.311454fc5a8df04ab12f32f8cf47e92e.js"
+  "/covid19-web/precache-manifest.6238515374120f921943db02a30752a2.js"
 );
 
 self.addEventListener('message', (event) => {
